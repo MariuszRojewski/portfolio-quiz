db61cade457836f14963fd39c54659d2be4e4eb5
Mariusz Rojewski <rojewski@webkon.eu>
2023-05-06 13:04:54 +0200

    Losowanie pytań i odpowedzi działa - brakuje stanu dla przycisków

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StartPage from "./components/StartPage";
+import QuestionsAndAnswears from "./components/QuestionsAndAnswears";
 
 // API
 //https://opentdb.com/api.php?amount=5&category=20
@@ -22,26 +23,20 @@ export default function App() {
     setStart(!start);
   }
 
-  let questions = null;
-
-  if (data.length > 1) {
-    questions = data.map((item, index) => {
-      return (
-        <div className="question" key={index}>
-          <h2>{item.question}</h2>
-        </div>
-      );
-    });
+  // zwraca kliknięte id, trzeba teraz jakoś smienić stan
+  // albo trzeba ddoać stata w środku, nie wiem... - ale się dowiem :)
+  function selectAnswears(id) {
+    console.log(id);
   }
 
   return (
     <>
       <div className="wrapper">
         {start ? (
-          questions === null ? (
-            ""
+          data.length > 1 ? (
+            <QuestionsAndAnswears data={data} selectAnswears={selectAnswears} />
           ) : (
-            questions
+            ""
           )
         ) : (
           <StartPage onClick={handleStart} />
---
+++ src/components/QuestionsAndAnswears.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { nanoid } from "nanoid";
+
+export default function QuestionsAndAnswears({ data, selectAnswears }) {
+  let questions = null;
+  console.log(selectAnswears);
+
+  if (data.length > 1) {
+    questions = data.map((item, index) => {
+      const allAnswears = [];
+
+      item.incorrect_answers.forEach((incorrect) =>
+        allAnswears.push({
+          text: incorrect,
+          realAnswear: false,
+          id: nanoid(),
+        })
+      );
+      allAnswears.push({
+        text: item.correct_answer,
+        realAnswear: true,
+        id: nanoid(),
+      });
+
+      const mapedAnswears = allAnswears.map((answear, index) => {
+        return (
+          <button
+            className="question--answear"
+            key={index}
+            onClick={() => selectAnswears(answear.id)}
+          >
+            {answear.text}
+          </button>
+        );
+      });
+
+      return (
+        <div className="question" key={index}>
+          <h2 className="question--header">{item.question}</h2>
+          <div className="question--all-answears">{mapedAnswears}</div>
+        </div>
+      );
+    });
+  }
+
+  return questions;
+}
